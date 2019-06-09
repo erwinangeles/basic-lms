75a6d3febe3b1233468ae78f75f2665eba375c38
erwinangeles <51410283+erwinangeles@users.noreply.github.com>
2019-06-09 12:07:40 -0700

    Update tutorial.blade.php

--- resources/views/courses/tutorial.blade.php
+++ resources/views/courses/tutorial.blade.php
@@ -46,7 +46,7 @@
         <div class="container d-flex justify-content-between">
             <a href="/" class="navbar-brand d-flex align-items-center">
                 <strong>LMSEANG</strong>
-                <a class="btn btn-sm btn-primary" href="/">Dashboard</a>
+                <a class="btn btn-sm btn-primary" href="/public">Dashboard</a>
             </a>
         </div>
     </div>
@@ -99,7 +99,7 @@
                 <div class="lesson-scroller-item active">
                     <a href="/course/{{$course->course_slug}}">
                         <div class="image-container" data-watch-height="" style="min-height: 134px;">
-                            <img alt="" class="media-object img-responsive" src="/images/getting-started.jpg">
+                            <img alt="" class="media-object img-responsive" src="public/images/getting-started.jpg">
                             <div class="lesson-title">
                                 <span class="lesson-module-number">1</span> <span class="text-uppercase">Getting Started</span>
                             </div></div></a>
@@ -109,7 +109,7 @@
                     <div class="lesson-scroller-item active">
                         <a href="/course/{{$course->course_slug}}/{{$module->module_slug}}">
                             <div class="image-container" data-watch-height="" style="min-height: 134px;">
-                                <img alt="" class="media-object img-responsive" src="/images/{{$module->module_image}}">
+                                <img alt="" class="media-object img-responsive" src="public/images/{{$module->module_image}}">
                                 <div class="lesson-title">
                                     <span class="lesson-module-number">{{ $loop->iteration+1 }}</span> <span class="text-uppercase">{!! $module->module_name !!}</span>
                                 </div></div></a>
@@ -119,7 +119,7 @@
                 <div class="lesson-scroller-item active">
                     <a href="/{{$course->course_slug}}/summary">
                         <div class="image-container" data-watch-height="" style="min-height: 134px;">
-                            <img alt="" class="media-object img-responsive" src="/images/summary.png">
+                            <img alt="" class="media-object img-responsive" src="public/images/summary.png">
                             <div class="lesson-title">
                                 <span class="lesson-module-number"></span> <span class="text-uppercase">Summary</span>
                             </div></div></a>
