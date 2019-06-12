0a913c717691cdeb88504273c311cb994db46924
Erwin <erwin@eang.media>
2019-06-12 16:29:56 -0700

    updated views

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,8 @@
     <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         <h1 class="page-header">Welcome to LMS Admin</h1>
 
-        <p>This is the dashboard for the admin panel. To edit courses and modules click on the menu on the left.</p>
+        <p>This is the dashboard for the admin panel. To edit courses and modules click on the Manage Courses menu item or click the button below</p>
         <p>To go back to the front-end view, click on the Dashboard button above</p>
+      <a href="{{route('admin.courses.index')}}"><button type="button" class="btn btn-primary">Manage Courses</button></a>
     </div>
     @endsection
